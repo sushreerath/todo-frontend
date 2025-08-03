eea6e6e1c1518a4c7aa03e7df838ddb33a714feb
sushreerath <rathsuman503@gmail.com>
2025-08-03 11:39:36 +0530

    Update vite.config.js and frontend deployment files

--- src/Services/AuthServices.jsx
+++ src/Services/AuthServices.jsx
@@ -1,14 +1,17 @@
 import axios from "axios";
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL ; // or correct port
 
+
 const loginUser = (data) => {
-  return axios.post(`${API_BASE_URL}/user/login`, data);
+  return axios.post(`${API_BASE_URL}/user/login`, data);
 };
 
+
 const registerUser = (data) => {
-  return axios.post(`${API_BASE_URL}/user/register`, data);
+  return axios.post(`${API_BASE_URL}/user/register`, data);
 };
+
 
 
 const AuthServices = { registerUser, loginUser };
-export default AuthServices;
+export default AuthServices;  
